98098c1ad2139e24e56ce65a1258f77d3ac14227
Monir <hossinmonir681@gmail.com>
2023-05-19 12:16:49 +0600

    nabvar and footer added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer/Footer';
+import Nabbar from '../Shared/Footer/Nabbar/Nabbar';
+
+const Main = () => {
+    return (
+        <div>
+            <Nabbar></Nabbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
