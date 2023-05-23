0fac682ef5b1aec9e3a3591dfa83bfc2343e0889
Monir <hossinmonir681@gmail.com>
2023-05-23 23:05:46 +0600

    unwanted data deleted

--- src/Pages/AllToys/AllToys.jsx
+++ src/Pages/AllToys/AllToys.jsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2'
+import { AuthContext } from '../../Provider/AuthProvider';
 
 
 const AllToys = () => {
+
+    const {user} = useContext(AuthContext);
    
     const [toys, setToys] = useState(null)
-    const [search, setSearch] = useState('')
    useEffect(()=>{
     fetch('https://kidoozie-car-server.vercel.app/products')
     .then(res => res.json())
@@ -14,24 +17,32 @@ const AllToys = () => {
     })
    },[])
 
-   const handleSearch =()=>{
-    fetch(`https://kidoozie-car-server.vercel.app/${search}`)
-    .then(res => res.json())
-    .then(data =>{
-        setToys(data)
-    })
-   }
+   const notLogin =()=>{
+       
+    if(!user){
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+          })
+          
+          Toast.fire({
+            icon: 'warning',
+            title: 'Without a login, you can not visit the single toy details page'
+          })
+    }
+
+}
 
     return (
         <div className='m-12'>
-            <div className=" p-2 text-center">
-          <input
-            onChange={(e) => setSearch(e.target.value)}
-            type="text"
-            className="border border-[#FF6799] rounded-full py-2"
-          />{" "}
-          <button className='bg-[#FF6799] py-2 px-8 rounded-full font-bold text-white' onClick={handleSearch}>Search</button>
-        </div>
+           
             <div className="overflow-x-auto">
                 <table className="table w-full">
                     {/* head */}
@@ -57,7 +68,7 @@ const AllToys = () => {
                                     <td>{toy.subCategory}</td>
                                     <td>$ {toy.price}</td>
                                     <td>{toy.quantity}</td>
-                                    <td><Link  to={`/productDetails/${toy._id}`} className="btn btn-error bg-[#FF6799] text-white font-bold">View Details</Link></td>
+                                    <td><Link onClick={notLogin}  to={`/productDetails/${toy._id}`} className="btn btn-error bg-[#FF6799] text-white font-bold">View Details</Link></td>
                                     </tr>
                                 )
                             }
