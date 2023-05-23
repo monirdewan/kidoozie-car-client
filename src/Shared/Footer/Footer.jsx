08262dfc5a93c8d84f7e09f6242d84036c411352
Monir <hossinmonir681@gmail.com>
2023-05-23 11:19:25 +0600

    footer section added

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,35 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from './../../assets/logo.png'
+import { FaFacebook, FaInstagram, FaRegMap } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
+import { TbBrandTwitter, TbPhonePause } from 'react-icons/tb';
+import { SlSocialFacebook } from 'react-icons/sl';
+import { CiYoutube } from 'react-icons/ci';
+import { AuthContext } from '../../Provider/AuthProvider';
+import { Link } from 'react-router-dom';
+
 
 const Footer = () => {
-    return (
-        <footer className="footer p-10 bg-base-200 text-base-content">
-        <div>
-          <img src={logo} alt="" />
-          <p>Kidoozie Car Store.<br/>A kids toy shop is a store that specializes in <br/>selling toys and car designed specifically for children.</p>
-        </div> 
-        <div>
-          <span className="footer-title">Services</span> 
-          <a className="link link-hover">Branding</a> 
-          <a className="link link-hover">Design</a> 
-          <a className="link link-hover">Marketing</a> 
-          <a className="link link-hover">Advertisement</a>
-        </div> 
-        <div>
-          <span className="footer-title">Company</span> 
-          <a className="link link-hover">About us</a> 
-          <a className="link link-hover">Contact</a> 
-          <a className="link link-hover">Jobs</a> 
-          <a className="link link-hover">Press kit</a>
-        </div> 
-        <div>
-          <span className="footer-title">Legal</span> 
-          <a className="link link-hover">Terms of use</a> 
-          <a className="link link-hover">Privacy policy</a> 
-          <a className="link link-hover">Cookie policy</a>
+  const {user} = useContext(AuthContext);
+  const navItems = <>
+        <Link className='text-lg' to='/'>Home</Link>
+        <Link className='text-lg' to='/alltoys'>All Toys</Link>
+       {
+        user && <>
+         <Link className='text-lg' to='/mytoy'>My Toys</Link>
+        <Link className='text-lg' to='/addtoy'>Add A Toy</Link>
+        </>
+       }
+        <Link className='text-lg' to='/about'>Blogs</Link>
+    </>
+  return (
+    <footer className="footer p-10 bg-base-200 text-base-content">
+      <div>
+        <img src={logo} alt="" />
+        <p>Kidoozie Car Store.<br />A kids toy shop is a store that specializes in <br />selling toys and car designed specifically for children.</p>
+        <div className='mt-4 '>
+          <div className='flex items-center gap-4 mb-2'><span className='text-3xl text-[#FF6799]'><FaRegMap /></span> <p className='text-lg'>Addresss: 1200 Abbot Kinney Nebraska USA</p></div>
+          <div className='flex items-center gap-4 mb-2'><span className='text-3xl text-[#FF6799]'><FiMail/></span> <p className='text-lg'>Email: hello@example.com</p></div>
+          <div className='flex items-center gap-4 mb-2'><span className='text-3xl text-[#FF6799]'><TbPhonePause /></span> <p className='text-lg'>Phone: (012) 345 6789</p></div>
         </div>
-      </footer>
-    );
+      </div>
+      
+      <div>
+        <span className="footer-title">Quick Links</span>
+        {navItems}
+      </div>
+      <div>
+        <span className="footer-title">Follow Us On</span>
+        <a className="link link-hover flex items-center text-lg mb-2"><SlSocialFacebook className='mr-4 text-[#FF6799] text-2xl'/> Facebook</a>
+        <a className="link link-hover flex items-center text-lg mb-2"><TbBrandTwitter className='mr-4 text-[#FF6799] text-2xl'/> Twitter</a>
+        <a className="link link-hover flex items-center text-lg mb-2"><FaInstagram className='mr-4 text-[#FF6799] text-2xl'/>Instagram</a>
+        <a className="link link-hover flex items-center text-lg mb-2"><CiYoutube className='mr-4 text-[#FF6799] text-2xl'/>Youtube</a>
+       
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
